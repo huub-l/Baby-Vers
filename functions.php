300b8d5cdd06c3c6939f59e8dbd1456f52306f73
Huub Lunter <info@studiolunter.nl>
2018-07-09 21:33:11 +0200

    Start

---
+++ functions.php
@@ -0,0 +1,14 @@
+<?php
+	 add_action( 'wp_enqueue_scripts', 'baby_vers_enqueue_styles' );
+	 function baby_vers_enqueue_styles() {
+ 		  wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
+ 		  }
+
+// class Suki_Pro {
+//
+// }
+
+
+
+
+ ?>
